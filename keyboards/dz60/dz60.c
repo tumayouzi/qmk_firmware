e4ee0f7e416be4d672827d39208f50b594987220
tumayouzi <tumayouzi@gmail.com>
2019-07-02 10:42:07 +0900

    dz60: disable capslock led setting

--- keyboards/dz60/dz60.c
+++ keyboards/dz60/dz60.c
@@ -21,12 +21,12 @@ void led_init_ports(void) {
   PORTB |= (1 << 2);
 }
 
-void led_set_kb(uint8_t usb_led) {
-    if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-        PORTB &= ~(1 << 2);
-    } else {
-        PORTB |= (1 << 2);
-    }
-
-    led_set_user(usb_led);
-}
+// void led_set_kb(uint8_t usb_led) {
+//     if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
+//         PORTB &= ~(1 << 2);
+//     } else {
+//         PORTB |= (1 << 2);
+//     }
+//
+//     led_set_user(usb_led);
+// }
